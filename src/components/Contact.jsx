5ee3f099d16ac27f6982dcd2fa6edfdce19c9d98
SushAN766 <sushanthbs766@gmail.com>
2025-05-02 01:10:25 +0530

    Contact component with updated form and layout

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,26 +4,34 @@ import { CONTACT } from '../constants'
 const Contact = () => {
   return (
     <div className="border-t border-stone-900 pb-20">
-        <h2
-        //whileInView={{opacity:1, y:0}}
-        //initial={{opacity:0, y:-100}}
-        //transition={{duration:1.5}}
-        className='my-10 text-center text-4xl'>Let's Connect</h2>
+        <motion.h2
+            whileInView={{opacity:1, y:0}}
+            initial={{opacity:0, y:-100}}
+            transition={{duration:1.5}}
+            className='my-10 text-center text-4xl'
+        >
+            Let's Connect
+        </motion.h2>
         <div className='text-center tracking-tighter'>
-            <p className="my-4">
-                {CONTACT.address}   
-            </p>
             
-            <p
-            //whileInView={{opacity:1, x:0}}
-            //initial={{opacity:0, x:100}}
-            //transition={{duration:1}}
-            className='my-4'>
+            <motion.p 
+                whileInView={{opacity:1, x:0}}
+                initial={{opacity:0, x:-100}}
+                transition={{duration:1}}
+                className="my-4">
+                {CONTACT.address}   
+            </motion.p>
+            
+            <motion.p
+                whileInView={{opacity:1, x:0}}
+                initial={{opacity:0, x:100}}
+                transition={{duration:1}}
+                className='my-4'>
                 {CONTACT.phoneNo}
-            </p>
-            <a href="#" className='border-b'>
-                {CONTACT.email}
-            </a>
+            </motion.p>
+            
+            <a href="#" className='border-b'>{CONTACT.email}</a>
+        
         </div>
     </div>
         
