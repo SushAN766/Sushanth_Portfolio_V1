0d1a159a364239f3ef29a69b4be10ceac8117fca
SushAN766 <sushanthbs766@gmail.com>
2025-05-02 01:19:55 +0530

    adjust Hero section resume link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -59,7 +59,7 @@ const Hero = () => {
             <motion.span 
               variants={childVariants}
               className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">
-                Web Developer
+                Full Stack Developer
             </motion.span>
             <motion.p 
               variants={childVariants}
