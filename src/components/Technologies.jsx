24101299b3eba040a71086f9a686e123aa582cff
SushAN766 <sushanthbs766@gmail.com>
2025-05-02 00:48:44 +0530

    Refactor Technologies component for improved structure and styling

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -5,125 +5,121 @@ import { RiReactjsLine, RiTailwindCssFill } from 'react-icons/ri'
 import { SiExpress, SiMongodb } from 'react-icons/si'
 import { TbBrandNextjs } from 'react-icons/tb'
 
+import { motion } from 'framer-motion'
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
+
 
 const Technologies = () => {
   return (
     <div className="pb-24">
-      <h2
-        //whileInView={{ opacity: 1, y: 0 }}
-        //initial={{ opacity: 0, y: -100 }}
-        //transition={{ duration: 1.5 }}
-        className="my-20 text-center text-4xl"
-      >
-        Technologies
-      </h2>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl">Technologies
+      </motion.h2>
 
       <div className="flex flex-wrap items-center justify-center gap-4">
-        <div
-          //variants={iconVariants(2.5)}
-          //initial="initial"
-          //animate="animate"
-          className="p-4"
-        >
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="p-4">
           <RiReactjsLine className="text-7xl text-cyan-400" />
-        </div>
+        </motion.div>
 
-        <div
-          //variants={iconVariants(2.8)}
-          //initial="initial"
-          //animate="animate"
+        <motion.div
+          variants={iconVariants(2.8)}
+          initial="initial"
+          animate="animate"
           className="p-4"
         >
           <TbBrandNextjs className="text-7xl" />
-        </div>
+        </motion.div>
 
-        <div
-          //variants={iconVariants(3.0)}
-          //initial="initial"
-          //animate="animate"
+        <motion.div
+          variants={iconVariants(3.0)}
+          initial="initial"
+          animate="animate"
           className="p-4"
         >
           <SiMongodb className="text-7xl text-green-500" />
-        </div>
+        </motion.div>
 
-        
-
-        <div
-          //variants={iconVariants(6)}
-          //initial="initial"
-          //animate="animate"
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
           className="p-4"
         >
           <FaNodeJs className="text-7xl text-green-500" />
-        </div>
+        </motion.div>
 
-        <div
-          //variants={iconVariants(4)}
-          //initial="initial"
-          //animate="animate"
-          className="p-4"
-        >
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="p-4">
           <BiLogoPostgresql className="text-7xl text-sky-700" />
-        </div>
+        </motion.div>
       
       </div>
       <div className="flex flex-wrap items-center justify-center gap-4">
          
-         <div 
-         //variants={iconVariants(3.2)} 
-         //initial="initial" 
-         //animate="animate" 
-         className="p-4"
-         >
+        <motion.div 
+          variants={iconVariants(3.2)} 
+          initial="initial" 
+          animate="animate" 
+          className="p-4"
+        >
           <DiPython className="text-7xl text-blue-500" />
-        </div>
+        </motion.div>
 
 
-      <div 
-         //variants={iconVariants(3.2)} 
-         //initial="initial" 
-         //animate="animate" 
-         className="p-4"
+        <motion.div 
+          variants={iconVariants(3.2)} 
+          initial="initial" 
+          animate="animate" 
+          className="p-4"
          >
           <DiJava className="text-7xl text-blue-500" />
-        </div>
+        </motion.div>
 
+        <motion.div 
+         variants={iconVariants(3.2)} 
+         initial="initial"
+         animate="animate"
+         className="p-4">
+          <DiGit className="text-7xl text-red-700" />
+        </motion.div>
 
+        <motion.div 
+         variants={iconVariants(3.2)} 
+         initial="initial" 
+         animate="animate"
+         className="p-4">
+          <RiTailwindCssFill className="text-7xl text-cyan-700" />
+        </motion.div>
 
-
-
-
-        {/* <div 
-         variants={iconVariants(3.0)} 
+        <motion.div 
+         variants={iconVariants(3.2)}
          initial="initial"
          animate="animate" 
          className="p-4">
-        <img src="../postman.png" alt="Postman" className="w-16 h-16 rounded-4xl" />
-        </div>*/}
-
-        <div 
-        //variants={iconVariants(3.2)} 
-       // initial="initial"
-        // animate="animate"
-        className="p-4">
-          <DiGit className="text-7xl text-red-700" />
-        </div>
-
-        <div 
-        //variants={iconVariants(3.2)} 
-        //initial="initial" 
-        //animate="animate"
-         className="p-4">
-          <RiTailwindCssFill className="text-7xl text-cyan-700" />
-        </div>
-
-        <div 
-        //variants={iconVariants(3.2)}
-         //initial="initial"
-        //animate="animate" 
-        className="p-4">
           <SiExpress className="text-7xl text-green-700" />
-        </div>
+        </motion.div>
 
       </div>
     </div>
